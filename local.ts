920ad864d2b2362bbe03e2cd015438408fdb4165
Grimmer <grimmer0125@gmail.com>
2017-12-27 13:09:26 +0800

    Delete some comments

--- local.ts
+++ local.ts
@@ -1,40 +1,15 @@
-const loadData = require('./lib/loadData');
+const loadData = require("./lib/loadData");
 
 (() => {
-  console.log('updateTWHouseLatestData start');
+  console.log("updateTWHouseLatestData start");
   // testUploadToFirebase();
   loadData.downloadLatest();
   // loadData.handle20171201();
   // loadData.loadSeasonData();
   // loadData.uploadTestDataToFirebase('2012S4-2017S3.json');
 
-  console.log('updateTWHouseLatestData end');
+  console.log("updateTWHouseLatestData end");
 
-
-  // require the Twilio module and create a REST client
-  // Twilio Credentials
-  // const accountSid = 'AC83651bf8e21c30b313a44ccb97db3688';
-  // const authToken = 'ee7f411bf34d3424bc8cd4c934193079';
-  // // const twilioClient = require('twilio')(accountSid, authToken);
-  // //
-  // // twilioClient.messages.create({to: '+886963052251', from: '+12016279052', body: 'Line bot has problems !!'}).then((message) => {
-  // //   console.log(message.sid);
-  // // });
-  //
-  // var accountSid = 'AC83651bf8e21c30b313a44ccb97db3688';
-  // // var authToken = 'your_auth_token';
-  //
-  // //require the Twilio module and create a REST client
-  // var client = require('twilio')(accountSid, authToken);
-  //
-  // client.messages.create({
-  //   to: "+886963052251",
-  //   from: "+15017250604",
-  //   body: "This is the ship that made the Kessel Run in fourteen parsecs?"
-  // }, function(err, message) {
-  //   console.log(message.sid);
-  // });
-  // return;
   // var admin = require("firebase-admin");
   //
   // var serviceAccount = require("./taiwanhouseprice-firebase-adminsdk-semoq-7b5e0677fa.json");
