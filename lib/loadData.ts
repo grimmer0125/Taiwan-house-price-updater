de29d1c79f68537697b9030af76835286d4cf2ce
Grimmer <grimmer0125@gmail.com>
2018-11-13 12:56:35 +0800

    Update for 2018S3 season data
    Using Format Document.

--- lib/loadData.ts
+++ lib/loadData.ts
@@ -1,113 +1,56 @@
-// const fs = require("fs");
-
 import * as fs from "fs";
-
-const admin = require("firebase-admin");
 import serviceAccount from "../taiwanhouseprice-firebase-adminsdk";
-// console.log(serviceAccount);
+const admin = require("firebase-admin");
 
-// const updateSeasonDataToFirebase = require("./testFirebase.js");
-// updateSeasonDataToFirebase();
-// return;
-
-exports.downloadLatest = downloadLatest;
-exports.handle20171201 = handle20171201;
-exports.loadSeasonData = loadSeasonData;
-exports.uploadTestDataToFirebase = uploadTestDataToFirebase;
-// return;
-
-// copy from https://github.com/grimmer0125/TWHousePriceReactNative/blob/dev/parser.js
 // import {parseHouseCSV} from './parser.js';
 const parser = require("./parser");
 const AdmZip = require("adm-zip");
 const iconv = require("iconv-lite");
 
-// another famous modules are request, request-progress, request-promise
+// other famous modules are request, request-progress, request-promise
 // https://stackoverflow.com/a/36348693/7354486
 const http = require("http");
-// const fetch = require('node-fetch');
 
-// import {storage} from './storageHelper.js'; storage.load & storage.save
+function uploadToFirebase(houseprice, dataPath = "/houseprice") {
+  // const fileName = "20171201.json";
+  // const houseprice = JSON.parse(fs.readFileSync(fileName, 'utf8'));
 
-// this url is not valid anymore
-// const dataURL = "http://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=F0199ED0-184A-40D5-9506-95138F54159A";
-// handle20171201();
-function handle20171201() {
-  const fileName = "1061201"; // +".zip";
-  const fullFileName = `${fileName}.zip` ;
-
-  // const dataURL = "http://plvr.land.moi.gov.tw//Download?type=zip&fileName=" + fullFileName;
-  // const file = fs.createWriteStream(fullFileName);
-  // const request = http.get(dataURL, function(response) {
-  //   response.pipe(file);
-  //
-  //   file.on('finish', function() {
-  //     file.close(()=>{
-  const zipFilePath = fullFileName; // dirs + fileName +".zip";
-  const uncompressedPath = `out/${fileName}`;
-  loadAndParse(true, zipFilePath, uncompressedPath, (result) => {
-    // what is the date, 20171211.TXT -> 2017-12-11
-    // date:result
-          console.log("result:", result)  ;
-
-          fs.writeFile("20171201.json", JSON.stringify(result, null, 2), (err) => {
-      if (err) {
-        return console.log(err);
-      }
-
-      console.log("The file was saved!");
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://taiwanhouseprice.firebaseio.com"
   });
-  //     });
-  //   });
-  // });
+  const defaultDatabase = admin.database();
+
+  defaultDatabase
+    .ref(dataPath)
+    .update(houseprice)
+    .then(() => {
+      console.log("upload house price to firebase ok");
+
+      // defaultDatabase.goOffline(); //<-put here will not terminate
+      // so use app().delete();
+      admin.app().delete();
+    });
+  // defaultDatabase.goOffline(); //<-put here will terminate earily
 }
 
 function uploadTestDataToFirebase(fileName) {
-  // const houseprice = { 2055: { L: { price: 12222 } } };
   // const fileName = "20171201.json";
   const houseprice = JSON.parse(fs.readFileSync(fileName, "utf8"));
-  admin.initializeApp({ credential: admin.credential.cert(serviceAccount), databaseURL: "https://taiwanhouseprice.firebaseio.com" });
-
-  const defaultDatabase = admin.database();
-
-  const dataPath = "/houseprice-test";
-  // const fileName = "20171201.json";
-  // const houseprice = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-  defaultDatabase.ref(dataPath).update(houseprice).then(() => {
-    console.log("upload testing house price ok");
-    // defaultDatabase.goOffline(); //<-put here will not terminal
-    // so use app().delete();
-    admin.app().delete();
-  });
-
-  // defaultDatabase.goOffline(); //<-put here will terminal
+  // uploadToFirebase(houseprice, "/houseprice-test")
+  uploadToFirebase(houseprice);
 }
 
-function uploadToFirebase(houseprice) {
-
-  const fullPath = __dirname;
-  admin.initializeApp({ credential: admin.credential.cert(serviceAccount), databaseURL: "https://taiwanhouseprice.firebaseio.com" });
-
-  const defaultDatabase = admin.database();
-
-  const dataPath = "/houseprice";
-  // const fileName = "20171201.json";
-  // const houseprice = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-  defaultDatabase.ref(dataPath).update(houseprice).then(() => {
-    console.log("upload house price to firebase ok");
-    admin.app().delete();
-  });
-}
-
-function downloadLatest() {
-  const fileName = "lvr_landcsv"; // +".zip";
+function downloadHouseData(specificURL = "", saveMode = false) {
+  const fileName = "lvr_landcsv";
   const fullFileName = `${fileName}.zip`;
 
-  const dataURL = `http://plvr.land.moi.gov.tw//Download?type=zip&fileName=${fullFileName}`;
-  const file = fs.createWriteStream(`/tmp/${fullFileName}`);
-  const request = http.get(dataURL, (response) => {
+  const dataURL = specificURL
+    ? specificURL
+    : `http://plvr.land.moi.gov.tw//Download?type=zip&fileName=${fullFileName}`;
+  const request = http.get(dataURL, response => {
     console.log("download ok!");
+    const file = fs.createWriteStream(`/tmp/${fullFileName}`);
     response.pipe(file);
     // https://stackoverflow.com/a/45007624/7354486
     file.on("finish", () => {
@@ -120,36 +63,50 @@ function downloadLatest() {
 
       const zipFilePath = `/tmp/${fullFileName}`; // dirs + fileName +".zip";
       const uncompressedPath = `${"/tmp/" + "out/"}${fileName}`;
-      loadAndParse(true, zipFilePath, uncompressedPath, (result) => {
-          console.log("load and parse result ok:");
-          console.log("next step is to upload to firebase or save it");
+      loadAndParse(true, zipFilePath, uncompressedPath, result => {
+        console.log("load and parse result ok:");
+        console.log("next step is to upload to firebase or save it");
 
-          // way1:
+        if (!saveMode) {
           uploadToFirebase(result);
+        } else {
+          fs.writeFile("latest.json", JSON.stringify(result, null, 2), err => {
+            if (err) {
+              return console.log(err);
+            }
 
-          // way2:
-          // fs.writeFile('latest.json', JSON.stringify(result, null, 2), (err) => {
-          //   if (err) {
-          //     return console.log(err);
-          //   }
-          //
-          //   console.log('The file was saved!');
-          //   process.exit();
-          // });
-        //});
-
+            console.log("The file was saved!");
+            process.exit();
+          });
+        }
       });
     });
   });
 }
 
-// loadSeasonData();
-function loadSeasonData() {
-  // let fileName = "2017S3";
-  const dirs = "season-data/";
+// this url is not valid anymore
+// const dataURL = "http://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=F0199ED0-184A-40D5-9506-95138F54159A";
+function load20171201ZipToJson() {
+  const fileName = "1061201";
+  const fullFileName = `${fileName}.zip`;
 
-  const resultFilePath = "result.json";
-  const tatalFileList = [
+  const zipFilePath = fullFileName;
+  const uncompressedPath = `out/${fileName}`;
+  loadAndParse(true, zipFilePath, uncompressedPath, result => {
+    console.log("result:", result);
+
+    fs.writeFile("20171201.json", JSON.stringify(result, null, 2), err => {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log("The file was saved!");
+    });
+  });
+}
+
+function load2012_2017Zip() {
+  const totalFileList = [
     "2017S3",
     "2017S2",
     "2017S1",
@@ -169,17 +126,30 @@ function loadSeasonData() {
     "2013S3",
     "2013S2",
     "2013S1",
-    "2012S4",
+    "2012S4"
   ];
-  const num_seaons = tatalFileList.length;
+  loadArchiveZipToJson(totalFileList);
+}
+
+function load2018S3Zip() {
+  const totalFileList = ["2018S3"];
+  loadArchiveZipToJson(totalFileList);
+}
+
+function loadArchiveZipToJson(totalFileList) {
+  const dirs = "season-data/";
+
+  const resultFilePath = "result.json";
+
+  const num_seaons = totalFileList.length;
 
   const aggregated_result = {};
 
-  for (const fileName of tatalFileList) {
+  for (const fileName of totalFileList) {
     const zipFilePath = `${dirs + fileName}.zip`;
     const uncompressedPath = `out/${fileName}`;
 
-    loadAndParse(false, zipFilePath, uncompressedPath, (result) => {
+    loadAndParse(false, zipFilePath, uncompressedPath, result => {
       if (result) {
         let date = "";
         if (fileName.indexOf("S1") > -1) {
@@ -197,13 +167,17 @@ function loadSeasonData() {
         const numOfResults = Object.keys(aggregated_result).length;
 
         if (numOfResults == num_seaons) {
-          fs.writeFile(resultFilePath, JSON.stringify(aggregated_result, null, 2), (err) => {
-            if (err) {
-              return console.log(err);
-            }
+          fs.writeFile(
+            resultFilePath,
+            JSON.stringify(aggregated_result, null, 2),
+            err => {
+              if (err) {
+                return console.log(err);
+              }
 
-            console.log("The file was saved!");
-          });
+              console.log("The file was saved!");
+            }
+          );
         } else {
           console.log("Just save season data:", result);
         }
@@ -212,214 +186,58 @@ function loadSeasonData() {
       }
     });
   }
-
-  // loadAndParse(zipFilePath, uncompressedPath, (result)=>{
-  //     if (result) {
-  //         fs.writeFile(resultFilePath, JSON.stringify(result, null, 2), function(err) {
-  //             if(err) {
-  //                 return console.log(err);
-  //             }
-  //
-  //             console.log("The file was saved!");
-  //         });
-  //     } else {
-  //         console.log("empty result data");
-  //     }
-  // });
 }
 
-// main();
-
-// if (RNFetchBlob) {
-//     dirs = RNFetchBlob.fs.dirs;
-//
-//     zipFilePath = dirs.DocumentDir + '/house.zip';
-//     unzipPath = dirs.DocumentDir + "/house";
-//     console.log("got the file path:", zipFilePath);
-// }
-
-// function saveResult(newData) {
-//     console.log("in saveResult");
-//
-//     if (newData) {
-//         fs.writeFile(resultFilePath, JSON.stringify(newData, null, 2), function(err) {
-//             if(err) {
-//                 return console.log(err);
-//             }
-//
-//             console.log("The file was saved!");
-//         });
-//     } else {
-//         console.log("empty result data");
-//     }
-// }
-
-// download and save
-// TODO change downloadFile to Node.js's fetch or load local file
-// function downloadFile_ReactNative(savePath) {
-//   // write file
-//   return RNFetchBlob.config({
-//     // add this option that makes response data to be stored as a file,
-//     // this is much more performant.
-//     //   fileCache : true,
-//     //   appendExt : 'png',
-//     path: savePath, // dirs.DocumentDir + '/house.zip',
-//   }).fetch('GET', dataURL, {
-//     // some headers ..
-
-//     // Beware that when using a file path as Image source on Android,
-//     // you must prepend "file://"" before the file path
-//     // imageView = <Image source={{ uri : Platform.OS === 'android' ? 'file://' + res.path()  : '' + res.path() }}/>
-//   });
-// }
-
-// unzip, work
-// TODO[done] change ZipArchive to Node.js version's unzip, adm-zip
 function unzip(sourceZipPath, targetPath) {
   const zip = new AdmZip(sourceZipPath);
-  zip.extractAllTo(targetPath,
-  /* overwrite */
-    true);
-
-  // return ZipArchive.unzip(zipFilePath, unzipPath);
+  zip.extractAllTo(
+    targetPath,
+    /* overwrite */
+    true
+  );
 }
 
-// copy from https://github.com/grimmer0125/TWHousePriceReactNative/
-// method 1. use load iconv-lite, but it does not work on react-native
-// current: 2. another way is to modify ios/android code of react-native-fetch-blob to read big5 encoding
-// TODO use Node'js way to read BIG5 file to replace RNFetchBlob.fs.readStream
 function readEachCSVFile(unzipPath, code, houseType, finishReadFun) {
   const readfilepath = `${unzipPath}/${code}_LVR_LAND_${houseType}.CSV`;
 
   console.log("try to read:", readfilepath);
 
-  const body = iconv.decode(fs.readFileSync(readfilepath), "Big5");
+  // const body = iconv.decode(fs.readFileSync(readfilepath), "Big5");
 
-  // handle data
-  console.log("total data length:", body.length);
-  // console.log(body);
-  finishReadFun(body);
+  try {
+    const body = fs.readFileSync(readfilepath, "utf8");
 
-  // let data = ''
-  // RNFetchBlob.fs.readStream(
-  //  encoding, should be one of `base64`, `utf8`, `ascii`
-  // readfilepath, `big5`, 1095000 should set large enough
-  //  file path
-  //  4K buffer size.
-  //  (optional) buffer size, default to 4096 (4095 for BASE64 encoded data)
-  //  when reading file in BASE64 encoding, buffer size must be multiples of 3.
-  // ).then((ifstream) => {
-  //     ifstream.open()
-  //     ifstream.onData((chunk) => {
-  //          when encoding is `ascii`, chunk will be an array contains numbers
-  //          otherwise it will be a string
-  //         data += chunk
-  //
-  //          [asciiArray addObject:[NSNumber numberWithChar:bytePtr[i]]];
-  //          when encoding is `ascii`, chunk will be an array contains numbers
-  //
-  //          console.log("chunk size:%s", chunk.length);
-  //
-  //          str = iconv.decode(new Buffer(chunk), 'Big5');
-  //          console.log("final:", str);
-  //     })
-  //     ifstream.onError((err) => {
-  //         console.log('oops-err', err);  not exist case and other cases
-  //     })
-  //     ifstream.onEnd(() => {
-  //
-  //         handle data
-  //         console.log("total data length:", data.length);
-  //         finishReadFun(data);
-  //     })
-  // })
+    // handle data
+    console.log("total data length:", body.length);
+    finishReadFun(body);
+  } catch (error) {
+    console.log("fail to read file, maybe not exist");
+    finishReadFun("");
+  }
 }
 
-// function parseHouseCSV(readFileFun, readAllCallback){
-//   readCallback = readAllCallback;
-//   console.log("start parseHouseCSV");
-//
-//   let NumOfCity = cityList.length;
-//
-//    n x 2 個非同步
-//   for (let i=0; i< NumOfCity; i++){
-//     let parser = new priceFileParser(cityList[i].code);
-//     parser.readAndCountSync(readFileFun);
-//   }
-//   console.log("loop all");
-// }
-
-// function downloadAndParse(dataCallback) {
 function loadAndParse(appendDate, sourceZipPath, unzipPath, dataCallback) {
-  // console.log("start to download");
-  //
-  // downloadFile().then((res) => {
-  //      the temp file path
-  //     console.log('The file saved to ', res.path())
-  //
-  //     return unzip();
-  // })
+  Promise.resolve("")
+    .then(() => {
+      unzip(sourceZipPath, unzipPath);
 
-  // unzip()
-  Promise.resolve("").then(() => {
-    unzip(sourceZipPath, unzipPath);
+      console.log("unzip completed!");
 
-    console.log("unzip completed!");
-    // return;
+      parser.parseHouseCSV(appendDate, unzipPath, readEachCSVFile, cityData => {
+        console.log("houseData:", cityData);
 
-    // comment temporarily
-    parser.parseHouseCSV(appendDate, unzipPath, readEachCSVFile, (cityData) => {
-      console.log("houseData:", cityData);
+        dataCallback(cityData);
+      });
+    })
+    .catch(error => {
+      console.log("unzip or parse error:");
 
-      // const newData = cityData.map(city=>{
-      //     let finalNum = 0;
-      //     if(city.price<0){
-      //         finalNum ="error";
-      //     } else if (city.price ==0) {
-      //         finalNum = "沒有交易";
-      //     } else {
-      //         finalNum = (Math.round(city.price)).toString().replace(/\B(?=(\d{3})+(?!\d))/g,
-      //               ",");
-      //     }
-      //
-      //     return city.name+":$"+finalNum;
-      // });
-      // console.log("new:", newData);
-
-      // React native cache specific.
-      // ref: https://github.com/grimmer0125/TWHousePriceReactNative/blob/dev/storageHelper.js
-
-      // TODO[done] change to use Node.js's's save file? saveResult
-      // storage.save(newData);
-
-      dataCallback(cityData);
+      console.log(error);
     });
-  }).catch((error) => {
-    console.log("unzip or parse error:");
-
-    console.log(error);
-  });
 }
 
-// this.onData  = this.onDataArrived.bind(this);
-// downloadAndParse(onDataArrived);
-//
-// const JSZip = require("jszip");
-// var zip = new JSZip();
-//
-// const bin = zipFilePath;
-// zip.loadAsync(bin)
-// .then(function (zip) {
-//     console.log(zip.files);
-//      folder1/folder2/folder3/file1.txt
-// });
-//
-//  with createFolders: true, all folders will be created
-// zip.loadAsync(bin, {createFolders: true})
-// .then(function (zip) {
-//     console.log(zip.files);
-//      folder1/
-//      folder1/folder2/
-//      folder1/folder2/folder3/
-//      folder1/folder2/folder3/file1.txt
-// });
+exports.downloadHouseData = downloadHouseData;
+exports.load2018S3Zip = load2018S3Zip;
+exports.load20171201ZipToJson = load20171201ZipToJson;
+exports.loadArchiveZipToJson = loadArchiveZipToJson;
+exports.uploadTestDataToFirebase = uploadTestDataToFirebase;
--- local.ts
+++ local.ts
@@ -1,36 +1,14 @@
 const loadData = require("./lib/loadData");
 
 (() => {
-  console.log("updateTWHouseLatestData start");
-  // testUploadToFirebase();
-  loadData.downloadLatest();
-  // loadData.handle20171201();
-  // loadData.loadSeasonData();
-  // loadData.uploadTestDataToFirebase('2012S4-2017S3.json');
+  console.log("updateTWHouseLatestData start1");
+
+  const dataURL =  "http://plvr.land.moi.gov.tw/DownloadHistory?type=history&fileName=20180421"; 
+  // loadData.downloadHouseData(dataURL);
+  // loadData.load2018S3Zip();
+  // 1.b loadData.load20171201ZipToJson();
+  loadData.uploadTestDataToFirebase('result.json');
 
   console.log("updateTWHouseLatestData end");
 
-  // var admin = require("firebase-admin");
-  //
-  // var serviceAccount = require("./taiwanhouseprice-firebase-adminsdk-semoq-7b5e0677fa.json");
-  //
-  // admin.initializeApp({credential: admin.credential.cert(serviceAccount), databaseURL: "https://taiwanhouseprice.firebaseio.com"});
-  //
-  // const defaultDatabase = admin.database();
-  //
-  // const dataPath = "/houseprice";  + result.uid;
-
-  // defaultDatabase.ref(dataPath).on('value', (snap) => {
-  // const houseValue = snap.val();
-  // });
-  // const dummy = {"2018-01-01":{price:100}};
-  // defaultDatabase.ref(dataPath).update(dummy).then(()=>{
-  // console.log("save house price ok");
-  // });
-
-  // const fileName = "20171201.json";
-  // const houseprice = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-  // defaultDatabase.ref(dataPath).update(houseprice).then(()=>{
-  //   console.log("save house price ok");
-  // });
 })();
